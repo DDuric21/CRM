ec7fe9c2ce32e280806148534731f5278e47d2b2
Domagoj Đurić <dogysipo@gmail.com>
2023-10-10 22:05:26 +0200

    Added DB context and initial data to test if it works

---
+++ Data/DbContext/CrmDbContext.cs
@@ -0,0 +1,41 @@
+﻿using Backend_API.Data.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend_API.Data.DbContext
+{
+    public class CrmDbContext : Microsoft.EntityFrameworkCore.DbContext // for unknown reason i need to explicitly define DbContext
+    {
+        public static CrmDbContext _context;
+
+        public CrmDbContext(DbContextOptions<CrmDbContext> options) : base(options)
+        {
+            _context = this;
+        }
+
+        public static CrmDbContext CreateNewContext()
+        {
+            if (_context != null)
+            {
+                return _context;
+            }
+            else
+            {
+                var builder = WebApplication.CreateBuilder();
+                var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+                var optionsBuilder = new DbContextOptionsBuilder();
+                var options = (DbContextOptions<CrmDbContext>)optionsBuilder.UseSqlServer(connectionString).Options;
+
+                _context = new CrmDbContext(options);
+
+                return _context;
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Customer>();
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
---
+++ Data/Model/Customer.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Backend_API.Data.Model
+{
+    public class Customer
+    {
+        [Key]
+        public long Id { get; set; }
+        public string? Name { get; set; }
+        public DateTime Birthday { get; set; }
+    }
+}
