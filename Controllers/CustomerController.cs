b4fa0b5134edd0c0d4cec11d1ca1916ca2a95274
Domagoj Đurić <dogysipo@gmail.com>
2023-10-10 22:18:00 +0200

    Added simple Get all rq

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,25 @@
+﻿using Backend_API.Data.DbContext;
+using Backend_API.Data.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend_API.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private readonly CrmDbContext _context;
+
+        public CustomerController(CrmDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        [Route("/Customer")]
+        public IEnumerable<Customer> GetAll()
+        {
+            var customer = _context.Customers.ToList();
+
+            return customer;
+        }
+    }
+}
