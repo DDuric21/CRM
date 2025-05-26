0dae1f77756949ea7ce088721228acd507cb5c46
Domagoj Đurić <dogysipo@gmail.com>
2025-05-27 01:29:00 +0200

    Creating ActionResult

---
+++ UI/Helpers/ActionResult.cs
@@ -0,0 +1,38 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace UI.Helpers
+{
+    public class ActionResult<T> where T : class
+    {
+        public bool IsSuccess { get; set; }
+
+        public string? ErrorMessage { get; set; }
+
+        public T? Data { get; set; }
+
+        public ActionResult(bool isSuccess, string errorMessage, T? data = null)
+        {
+            IsSuccess = isSuccess;
+            ErrorMessage = errorMessage;
+            Data = data;
+        }
+
+        public ActionResult(string errorMessage, T? data = null)
+        {
+            IsSuccess = false;
+            ErrorMessage = errorMessage;
+            Data = data;
+        }
+
+        public ActionResult(bool isSuccess)
+        {
+            IsSuccess = isSuccess;
+        }
+
+        public ActionResult(T data)
+        {
+            IsSuccess = true;
+            Data = data;
+        }
+    }
+}
