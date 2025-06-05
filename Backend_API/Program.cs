6d3ea5c3316e0c74b48fc5a75ed1b90f2a5416b4
Domagoj Đurić <dogysipo@gmail.com>
2025-06-05 22:54:15 +0200

    Removing testing part from Program.cs

--- Backend_API/Program.cs
+++ Backend_API/Program.cs
@@ -1,4 +1,3 @@
-using AutoMapper;
 using Backend_API.Handlers;
 using Backend_API.Logging;
 using Backend_API.Startup;
@@ -10,9 +9,6 @@ ApplicationConfigurationSetup.ConfigurateLogger(builder);
 DependencyInjectionSetup.RegisterServices(builder);
 
 var app = builder.Build();
-
-//var mapper = app.Services.GetRequiredService<IMapper>();
-//mapper.ConfigurationProvider.AssertConfigurationIsValid();
 
 var httpContextAccessor = app.Services.GetRequiredService<IHttpContextAccessor>();
 DynamicLogger.Configure(httpContextAccessor);
